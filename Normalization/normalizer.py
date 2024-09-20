d07f6cfd3aa35fb24bce501ef6a5ee7982b523ec
AdairGarcia <agarciat1800@alumno.ipn.mx>
2024-09-20 17:00:33 -0600

    normalizer completed!

--- Normalization/normalizer.py
+++ Normalization/normalizer.py
@@ -1,28 +1,38 @@
-import spacy
+import spacy as sp
 from stop_words import get_stop_words
+import pandas as pd
 
-def tokenizer(string):
-    nlp = spacy.load("es_core_news_sm")
+def normalizer(string):
+    if not isinstance(string, str):
+        return ""
+
+    nlp = sp.load("es_core_news_sm")
     doc = nlp(string)
+
+    no_stop_words_string = ""
+
     for token in doc:
-        print(token.text)
+        if (not token.lemma_.lower() in stop_words_spanish) and (token.pos_ != "PUNCT"):
+            no_stop_words_string += token.lemma_ + " "
+        elif token.pos_ == "PUNCT" and token.i == len(doc) - 1:
+            no_stop_words_string = no_stop_words_string[:-1]
+            no_stop_words_string += token.lemma_
 
-    return doc
+    return no_stop_words_string
 
-def delete_stop_words(doc):
-    # we are working tutututu
-    for entity in doc.ents:
-        print(entity.text, entity.label_)
-    return doc
+def clean_data(df):
+    empty_df = df[0:0]
+    empty_df.to_csv('raw data corpus.csv', index=False)
+
+def add_data():
+    df = pd.read_csv('../Script/raw data corpus.csv')
+    df['Title'] = df['Title'].apply(normalizer)
+    df['Content'] = df['Content'].apply(normalizer)
+
+    df.to_csv('normalized data corpus.csv', index=False)
 
 
 if __name__ == '__main__':
-    texto = "Este es un ejemplo simple de cómo eliminar las stop words de un texto."
     stop_words_spanish = get_stop_words('spanish')
 
-    tokenizer(texto)
-
-
-
-
-
+    add_data()
